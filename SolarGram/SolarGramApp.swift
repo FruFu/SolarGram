fb62936d914e18890420f589a34eed184ae333f9
edward <edwardf1@uci.edu>
2023-02-27 20:56:13 -0800

    Initial Commit

---
+++ SolarGram/SolarGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarGramApp.swift
+//  SolarGram
+//
+//  Created by Edward Fu on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SolarGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
